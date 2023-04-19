8411ad9915bdcb60035ca28647a865811b5c8aa3
Daniil Smirnov <daniil.smirnov@nordclan.com>
2023-04-19 16:36:13 +0300

    feat: implement ui PhotosViewController.

---
+++ authTestTask/authTestTask/Sources/Modules/PhotosModule/PhotoCollectionView.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoCollectionView.swift
+//  authTestTask
+//
+//  Created by Daniil on 19.04.2023.
+//
+
+import Foundation
---
+++ authTestTask/authTestTask/Sources/Modules/PhotosModule/PhotoCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoCollectionViewCell.swift
+//  authTestTask
+//
+//  Created by Daniil on 19.04.2023.
+//
+
+import Foundation
