b671b0e17e9ed36b84dc9a5085c987e7e47f91c6
Daniil Smirnov <daniil.smirnov@nordclan.com>
2023-04-19 15:50:12 +0300

    feat: Implement loginVC ui.

--- authTestTask/authTestTask/Sources/Application/AppDelegate.swift
+++ authTestTask/authTestTask/Sources/Application/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		
 		window = UIWindow(frame: UIScreen.main.bounds)
 		
-		let rootVC = ViewController()
+		let rootVC = LoginViewController()
 		window?.rootViewController = rootVC
 		window?.overrideUserInterfaceStyle = .light
 		window?.makeKeyAndVisible()
---
+++ authTestTask/authTestTask/Sources/Modules/LoginModule/LoginViewController.swift
@@ -0,0 +1,71 @@
+//
+//  LoginViewController.swift
+//  authTestTask
+//
+//  Created by Daniil on 19.04.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+	
+	// MARK: - Views
+	private let loginButton: UIButton = {
+		let button = UIButton(type: .system)
+		button.translatesAutoresizingMaskIntoConstraints = false
+		button.setTitle("Вход через VK", for: .normal)
+		button.titleLabel?.font = .systemFont(ofSize: 18)
+		button.backgroundColor = .black
+		button.tintColor = .white
+		button.layer.cornerRadius = 12
+		
+		return button
+	}()
+	
+	private let mobileUpLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.text = "Mobile Up\nGallery"
+		label.font = UIFont(name: "SF Pro Text Bold", size: 44)
+		label.numberOfLines = 2
+
+		return label
+	}()
+	
+	// MARK: - Properties
+	
+	// MARK: - Initialize
+	
+	// MARK: - Life cycle
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .red
+		configureView()
+		setupHierarchy()
+		setupLayout()
+	}
+}
+
+extension LoginViewController {
+	private func configureView() {
+		view.backgroundColor = .systemBackground
+	}
+	
+	private func setupHierarchy() {
+		[loginButton, mobileUpLabel].forEach { view.addSubview($0) }
+	}
+	
+	private func setupLayout() {
+		NSLayoutConstraint.activate([
+			mobileUpLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+			mobileUpLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 170),
+			
+			loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+			loginButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -42),
+			loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+			loginButton.heightAnchor.constraint(equalToConstant: 52)
+		
+		])
+	}
+}
