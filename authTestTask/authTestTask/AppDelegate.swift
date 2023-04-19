756bfcfb961d0957fc271cc0b7cf097ded885bf4
Daniil Smirnov <daniil.smirnov@nordclan.com>
2023-04-19 13:53:21 +0300

    feat: Prepare proj to work.

---
+++ authTestTask/authTestTask/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  authTestTask
+//
+//  Created by Daniil on 19.04.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+var window: UIWindow?
+	
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		
+		window = UIWindow(frame: UIScreen.main.bounds)
+		
+		let rootVC = ViewController()
+		window?.rootViewController = rootVC
+		window?.overrideUserInterfaceStyle = .light
+		window?.makeKeyAndVisible()
+		
+		return true
+	}
+}
+
